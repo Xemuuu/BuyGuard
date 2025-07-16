5f2e68ca37986522d398a9d8bb72b38812af7d9a
Kaczucha24 <krupakonrad24@gmail.com>
2025-07-16 11:48:19 +0200

    Update authRoutes.js

--- backend/BuyGuard.Api/bcknd/routes/authRoutes.js
+++ backend/BuyGuard.Api/bcknd/routes/authRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { login } = require('../controllers/authController');
+const { login, getMe } = require('../controllers/authControllers');
 const { authorizeRoles } = require('../middleware/authmiddleware');
 
 router.post('/login', login);
-
+router.get('/me', authenticateToken, getMe);
 
 router.get('/admin-only', authorizeRoles('admin'), (req, res) => {
   res.json({ message: 'Witaj adminie!' });
